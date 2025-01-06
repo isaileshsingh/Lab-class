6503a098edba7d0d96f592eb0202ea69ce3b9b36
Sailesh Singh <104683889+isaileshsingh@users.noreply.github.com>
2025-01-06 13:45:47 +0545

    Create singlyLinkedList.c
    This C program implements a basic singly linked list with the following operations:
    
    -> Creation of a new node: Creates a new node with the given data and returns a pointer to it.
    -> Insertion at beginning: Inserts a new node at the beginning of the list.
    -> Insertion at end: Inserts a new node at the end of the list.
    -> Deletion from beginning: Deletes the first node from the list.
    -> Deletion from end: Deletes the last node from the list.
    -> Printing the list: Prints the data of all nodes in the list.

---
+++ DSA/singlyLinkedList.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for the singly linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning of the list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Function to delete a node from the beginning of the list
+void deleteFromBeginning(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    free(temp);
+}
+
+// Function to delete a node from the end of the list
+void deleteFromEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty!\n");
+        return;
+    }
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    free(temp);
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Insert nodes
+    insertAtEnd(&head, 1);
+    insertAtBeginning(&head, 2);
+    insertAtEnd(&head, 3);
+
+    printf("Linked List: ");
+    printList(head);
+
+    // Delete nodes
+    deleteFromBeginning(&head);
+    deleteFromEnd(&head);
+
+    printf("Linked List after deletion: ");
+    printList(head);
+
+    return 0;
+}
